6baf8e7c0dfec2c43887206830886e84c2c6bdaa
franciscocheleno <francisco.c.heleno@ua.pt>
2024-03-05 09:34:39 +0000

    Create ddl.c, ddl.h, main.c and first update ddl.c

---
+++ ddl.c
@@ -0,0 +1,46 @@
+/**
+ * \file:   ddl.c
+ * \author: Francisco Heleno <francisco.c.heleno@ua.pt> and Hugo Carola <hugocarola@ua.pt>
+ *
+ * \date 5, March, 2024, 9:12 AM
+ * \brief 
+ * 
+ */
+
+#include "ddl.h"
+/**
+ * @brief Initializes the temperature vector with zeros
+ * @param temperatures Pointer to the temperature vector
+ */
+void MyDLLInit()
+{
+	return;
+}
+
+/**
+ * @brief Initializes the temperature vector with zeros
+ * @param temperatures Pointer to the temperature vector
+ */
+void MyDLLInsert() 
+{
+	return;
+}
+
+/**
+ * @brief Initializes the temperature vector with zeros
+ * @param temperatures Pointer to the temperature vector
+ */
+void MyDLLRemove() 
+{
+	return;
+}
+
+/**
+ * @brief Initializes the temperature vector with zeros
+ * @param temperatures Pointer to the temperature vector
+ */
+void MyDLLFind_Next_Previous() 
+{
+	return;
+}
+
